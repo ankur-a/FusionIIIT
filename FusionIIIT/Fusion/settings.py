a4df2b680882bcef56a529da4a1a470c48b0f13b
ankurcode <ankur977@gmail.com>
2018-03-17 17:01:29 +0530

    New UI to office of students

--- FusionIIIT/Fusion/settings.py
+++ FusionIIIT/Fusion/settings.py
@@ -150,7 +150,7 @@ WSGI_APPLICATION = 'Fusion.wsgi.application'
 DATABASES = {
     'default':
         {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.sqlite3',
             'NAME': 'fusion',
             'USER': 'root',
             'PASSWORD': '',
